3b5ab3bfc7ac5b8029050409a90a0832a2b68349
daviddavid97 <zhangxiyinzxy@gmail.com>
2017-05-17 14:12:36 +0800

    o

---
+++ new.cpp
@@ -0,0 +1,30 @@
+//
+// Created by zxy on 4/26/17.
+//
+
+#include <cstring>
+
+class CMyString
+{
+public:
+    CMyString(char* pData = NULL);
+    CMyString(const CMyString& str);
+    ~CMyString(void);
+
+private:
+    char* m_pData;
+};
+
+CMyString& CMyString::operator = (const CMyString &str)
+{
+    if(this != &str)
+    {
+        CMyString strTemp(str);
+
+        char* pTemp = strTemp.m_pData;
+        strTemp.m_pData = m_pData;
+        m_pData = pTemp;
+    }
+
+    return *this;
+}
